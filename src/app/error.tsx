cac96fbe0e64eee06140cf21dfcf0e0b0c8b3220
pulak-sarmah <pulaksarmah333@gmail.com>
2024-03-29 22:44:10 +0530

    add error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -4,7 +4,7 @@ export default function ErrorPage() {
   return (
     <div className="space-y-3 text-center">
       <h1 className="text-2xl font-bold">Error</h1>
-      <p>Sorry, something went wrong. Please try again later.</p>
+      <p>Something went wrong. Please try again later.</p>
     </div>
   );
 }
